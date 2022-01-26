9e484bd3202806c555b63b12f31cdaf7bece9561
StevenM <stevenbmendoza@gmail.com>
2022-01-25 22:17:47 -0800

    initial repo, html skeleton linked to js, js initial

---
+++ assets/js/game.js
@@ -0,0 +1,7 @@
+var playerName = window.prompt("What is your robot's name?");
+// 
+console.log("Your robot's name is " + playerName +" the Robot!");
+function fight() {
+    window.alert('The fight has begun!');
+}
+// fight();
